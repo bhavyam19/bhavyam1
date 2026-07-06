a5151dd44cc217352df692c11303f5945cb2a397
bhavyam19 <bhavyamgarg19@gmail.com>
2026-07-06 13:55:57 +0530

    Update 2903-find-indices-with-index-and-value-difference-i.cpp

--- 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.cpp
+++ 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        // Brute Force Approach (Due To less constraints the code has the time complexity of 0ms)
         for(int i=0;i<nums.size();i++){
             for (int j=i;j<nums.size();j++){
                 if(abs(i-j)>=indexDifference && abs(nums[i]-nums[j])>=valueDifference){
